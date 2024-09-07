d91070f7eb69a192935f32b5e54bbd1e267f0d3f
Greg Johnston <gregmj.sco@gmail.com>
2024-09-07 16:26:09 +0900

    Add Nav with logo placeholder and styled button

--- styled-blog/src/components/Header.jsx
+++ styled-blog/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyledHeader } from './styles/Header.styled'
+import { StyledHeader, Nav, Logo } from './styles/Header.styled'
+import { Container } from './styles/Container.styled'
+import { Button } from './styles/Button.styled'
 
 export default function Header() {
   return (
     <StyledHeader>
-        <h1>Blog</h1>
+        <Container>
+            <Nav>
+                <Logo src='./images/logo.svg' />
+                <Button> Posts</Button>
+            </Nav>
+            <h1>Blog</h1>
+        </Container>
     </StyledHeader>
   )
 }

ae1821a5e3fa491a8b1c3e85c1ba5ee368c37aa6
Greg Johnston <gregmj.sco@gmail.com>
2024-09-08 17:24:43 +0900

    Create Flex styled component

--- styled-blog/src/components/Header.jsx
+++ styled-blog/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyledHeader, Nav, Logo } from './styles/Header.styled'
 import { Container } from './styles/Container.styled'
 import { Button } from './styles/Button.styled'
+import { Flex } from './styles/Flex.styled'
 
 export default function Header() {
   return (
@@ -13,6 +14,14 @@ export default function Header() {
             </Nav>
             <h1>Blog</h1>
         </Container>
+
+        <Flex>
+            <div>
+                <h1>
+                    Blog landing hero section header
+                </h1>
+            </div>
+        </Flex>
     </StyledHeader>
   )
 }

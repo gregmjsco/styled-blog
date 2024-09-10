fe853d5a76c8d14a9e1b27817a39ca0f6a28b7b5
Greg Johnston <gregmj.sco@gmail.com>
2024-09-10 11:47:11 +0900

    Create Card component from content.js

---
+++ styled-blog/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Card({item: {id, title, body, image}}) {
+  return (
+    <div>
+        <div>
+            <h2>{title}</h2>
+            <p>{body}</p>
+        </div>
+
+        <div>
+            <img src={`../../public/images/${image}`} alt="" />
+        </div>
+    </div>
+  )
+}
---
+++ styled-blog/src/content.js
@@ -0,0 +1,22 @@
+const content = [
+    {
+        id: 1,
+        title: 'Blog Post 1 Card Title',
+        body: 'Body text for the card which will give a summary of the blog post content',
+        image: 'vite.svg',
+    },
+    {
+        id: 2,
+        title: 'Blog Post 2 Card Title',
+        body: 'Body text for the card which will give a summary of the blog post content',
+        image: 'vite.svg',
+    },
+    {
+        id: 3,
+        title: 'Blog Post 3 Card Title',
+        body: 'Body text for the card which will give a summary of the blog post content',
+        image: 'vite.svg',
+    },
+]
+
+export default content 
